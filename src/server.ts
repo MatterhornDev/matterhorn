9fadce4f16a2f00424c31c8af6f677afc987e076
Bhatt, Pankaj <Pankaj.X.Bhatt@td.com>
2019-02-28 09:40:40 -0500

    Fastify Swagger implementaion has been done with Matterhorn.
    Swagger interface will be available on the same port as that of Main Application with a path /documentation E.g. http://localhost:3000/documentation
    A sample path /user/{id} has been created where a user can post a Object (user creation object as per the schema )and corresponding result will be returned.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import * as Fastify from 'fastify'
+import { configureSwagger } from './plugins'
 
 export default function createServer(opts?: Fastify.ServerOptions) {
   const fastify = Fastify(opts)
+
+  configureSwagger(fastify); 
 
   /* eslint-disable-next-line no-unused-vars */
   fastify.get('/', async (request, reply) => {
@@ -11,6 +14,63 @@ export default function createServer(opts?: Fastify.ServerOptions) {
   /* eslint-disable-next-line no-unused-vars */
   fastify.get('/twitter', async (request, _reply) => {
     return { twitterHandle: request.query.handle }
+  })
+
+  // Fastify Swagger Test URI demonstration i.e. sample Path. 
+  fastify.post('/user/:id', {
+    schema: {
+      description: 'post some data',
+      tags: ['user'],
+      summary: 'A User Creation API',
+      params: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            description: 'user id'
+          }
+        }
+      },
+      body: {
+        type: 'object',
+        properties: {
+          comments: { type: 'string' },
+          user: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+              password: { type: 'string' }
+            }
+          }
+        }
+      },
+      response: {
+        201: {
+          description: 'Successful response',
+          type: 'object',
+          properties: {
+            message: { type: 'string' },
+            status: { type: 'string' },
+            username: { type: 'string' },
+          }
+        }
+      },
+      security: [
+        {
+          "apiKey": []
+        }
+      ]
+    }
+  }, (req, reply) => {
+
+    console.log("COMING HERE") ;
+    
+    return reply.code(201).send({
+      message: req.body.user.name + ' - Successfully Created',
+      status: 'success',
+      username: req.body.user.name
+    })
+
   })
 
   return fastify
