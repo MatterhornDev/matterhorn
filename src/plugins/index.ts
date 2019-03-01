7a7a6199ce8da4f6ceb64b24af85fb0901418f10
Bhatt, Pankaj <Pankaj.X.Bhatt@td.com>
2019-03-01 13:37:23 -0500

    Adding the LINTING RULES.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,42 +1,46 @@
-import * as FastifySwagger from 'fastify-swagger' ;
-import * as Fastify from 'fastify'
+import * as FastifySwagger from 'fastify-swagger' 
 
 export  function configureSwagger(fastifyInstance?: any ) {
     
-    fastifyInstance.register(FastifySwagger, {
-        routePrefix: '/documentation',
-        swagger: {
-          info: {
-            title: 'MatterHorn Test swagger',
-            description: 'testing the fastify swagger api',
-            version: '0.1.0'
-          },
-          externalDocs: {
-            url: 'https://swagger.io',
-            description: 'Find more info here'
-          },
-          schemes: ['http'],
-          consumes: ['application/json'],
-          produces: ['application/json'],
-          tags: [
-            { name: 'user', description: 'User related end-points' },
-            { name: 'code', description: 'Code related end-points' }
-          ],
-          securityDefinitions: {
-            apiKey: {
-              type: 'apiKey',
-              name: 'apiKey',
-              in: 'header'
-            }
-          }
-        },
-        exposeRoute: true
-      })
+  fastifyInstance.register(FastifySwagger, {
+    routePrefix: '/documentation',
+    swagger: {
+      info: {
+        title: 'MatterHorn Test swagger',
+        description: 'testing the fastify swagger api',
+        version: '0.1.0'
+      },
+      externalDocs: {
+        url: 'https://swagger.io',
+        description: 'Find more info here'
+      },
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'user', description: 'User related end-points' },
+        { name: 'code', description: 'Code related end-points' }
+      ],
+      securityDefinitions: {
+        apiKey: {
+          type: 'apiKey',
+          name: 'apiKey',
+          in: 'header'
+        }
+      }
+    },
+    exposeRoute: true
+  })
 
-  }; 
+} 
 
-  export default function configureOtherPlugins(fastifyInstance?: any ) {
+/*
+export default function configureOtherPlugins(fastifyInstance?: any ) {
     
-    // we can configure other plugins here. 
+  // we can configure other plugins here. 
 
-  }; 
+} */
+
+export function configureOtherThings() {
+
+}
