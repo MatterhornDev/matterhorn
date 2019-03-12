65c0a3b9b98036e7174c03ed1d3174ca2fe40886
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-03-11 23:00:54 -0400

    solved issue

--- tests/plugins.test.ts
+++ tests/plugins.test.ts
@@ -1,14 +1,12 @@
 import createServer from '../src/server'
 
-test('should return swager page on /documentation', () => {
+test('should return swager page on /documentation', async () => {
   const fastify = createServer()
 
-  fastify.ready(async () => {
-    try {
-      const response = await fastify.inject({ method: 'GET', url: '/documentation/static/index.html' })
-      expect(response.statusCode).toEqual(200)
-    } catch (error) {
-      expect(error).toBeFalsy()
-    }
-  })
+  try {
+    const response = await fastify.inject({ method: 'GET', url: '/documentation/static/index.html' })
+    expect(response.statusCode).toEqual(200)
+  } catch (error) {
+    expect(error).toBeFalsy()
+  }
 })
