09557d62b92830661dc6e01f0bc3af0e970b2175
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-04-01 17:29:56 -0400

    More verbose testing for fastify-jwt plugin

--- tests/plugins.test.ts
+++ tests/plugins.test.ts
@@ -4,23 +4,52 @@ const fastify = createServer()
 
 afterAll(() => fastify.close(() => null))
 
-test('should return swagger page on /documentation', async () => {
-  try {
-    const response = await fastify.inject({ method: 'GET', url: '/documentation/static/index.html' })
-    expect(response.statusCode).toEqual(200)
-  } catch (error) {
-    expect(error).toBeFalsy()
-  }
+describe('should test fastify-swagger plugin', () => {
+  test('should return swagger page on /documentation', async () => {
+    try {
+      const response = await fastify.inject({ method: 'GET', url: '/documentation/static/index.html' })
+      expect(response.statusCode).toEqual(200)
+    } catch (error) {
+      expect(error).toBeFalsy()
+    }
+  })
 })
 
-test('should issue token based on payload', async () => {
-  const payload = { user: 'Matterhorn' }
-
-  try {
-    const response = await fastify.inject({ method: 'POST', url: '/auth/issueToken', payload})
-    const token = JSON.parse(response.payload)['token']
-    expect(token).not.toBeNull()
-  } catch (error) {
-    expect(error).toBeFalsy()
-  }
+describe('should test fastify-jwt plugin', () => {
+  test('should issue and use valid token based on payload', async () => {
+    try {
+      let response = await fastify.inject({
+        method: 'POST',
+        url: '/auth/issueToken',
+        payload: {
+          user: 'Matterhorn'
+        }
+      })
+  
+      const token = JSON.parse(response.payload)['token']
+      expect(token).not.toBeNull()
+  
+      response = await fastify.inject({
+        method: 'POST',
+        url: '/auth/checkToken',
+        payload: {
+          token
+        }
+      })
+      const verified = JSON.parse(response.payload)['verified']
+      expect(verified).toBeTruthy()
+  
+      response = await fastify.inject({
+        method: 'GET',
+        url: '/auth/protectedRoute',
+        headers: {
+          authorization: `Bearer ${token}`
+        }
+      })
+      expect(JSON.parse(response.payload)).toMatchObject({ foo: 'bar' })
+  
+    } catch (error) {
+      expect(error).toBeFalsy()
+    }
+  })
 })
